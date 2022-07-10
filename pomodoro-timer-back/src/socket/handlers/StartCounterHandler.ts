054592b92b343d310107ec3bbe510675237644d4
Paulo Henrique <phferreirasouza106@gmail.com>
2022-07-10 14:35:05 -0300

    refactor(back): validations and sending messages when start a pomodoro

--- pomodoro-timer-back/src/socket/handlers/StartCounterHandler.ts
+++ pomodoro-timer-back/src/socket/handlers/StartCounterHandler.ts
@@ -1,7 +1,9 @@
 import { Server, Socket } from "socket.io";
-import { Counter } from "@common/Emiters";
+import { Counter, Message } from "@common/Emiters";
+import { IResponseDTO } from "@dtos/default/IResponseDTO";
 import { ICounterDTO } from "@dtos/ICounterDTO";
 import { IStartCounterDTO } from "@dtos/IStartCounterDTO";
+import { ICounterResponse } from "@dtos/response/ICounterResponseDTO";
 import { CounterPomodoro } from "@modules/CounterPomodoros";
 import { Pomodoro } from "@modules/Pomodoro";
 
@@ -14,28 +16,28 @@ class StartCounterHandler {
 
   execute(
     io: Server,
-    _: Socket,
+    socket: Socket,
     { counter, type, roomId }: IStartCounterDTO
   ): void {
     const pomoActive = this.pomoCounter.findById(roomId);
 
     if (pomoActive?.isRunning) {
+      socket.emit(Message, {
+        success: false,
+        message: "Pomodoro already started",
+      } as IResponseDTO);
       return;
     }
 
-    const emitCounter = (params: ICounterDTO) =>
-      io.to(roomId).emit(Counter, params);
-
-    if (pomoActive && !pomoActive.isRunning) {
-      pomoActive.start({
-        io: emitCounter,
-        roomId,
-        counter,
-        type,
-      });
-
-      return;
-    }
+    const emitCounter = ({ counter, type }: ICounterDTO) =>
+      io.in(roomId).emit(Counter, {
+        success: true,
+        message: `Pomodoro's ${type} started`,
+        data: {
+          type,
+          counter,
+        },
+      } as ICounterResponse);
 
     const pomodoro = new Pomodoro(roomId);
     pomodoro.start({
